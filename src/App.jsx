48e53ad1a38a4e0dc49e57a2ce5c26cd8a64b5bc
BINIYAM <biniyam3575@gmail.com>
2026-02-06 17:24:05 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import Fifth from './componentes/Fifth/Fifth.jsx'
+import First from './componentes/First/First.jsx'
+import Footer from './componentes/Footer/Footer.jsx'
+import Fourth from './componentes/Fourth/Fourth.jsx'
+import Header from './componentes/Header/Header.jsx'
+import Second from './componentes/Second/Second.jsx'
+import Sixth from './componentes/Sixth/Sixth.jsx'
+import Third from './componentes/Third/Third.jsx'
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <First/>
+      <Second/>
+      <Third/>
+      <Fourth/>
+      <Fifth/>
+      <Sixth/>
+      <Footer />
+    </>
+  )
+}
+
+export default App
